08d1e659c2b4de1664b52d19fac5288112be5574
Carla <cfpcarla@gmail.com>
2019-12-05 02:45:59 +0000

    implement movements the snake

--- client.js
+++ client.js
@@ -19,8 +19,6 @@ const connect = function() {
     console.log(data);
   });
 
-
-
   return conn;
 };
 
--- input.js
+++ input.js
@@ -1,13 +1,24 @@
+let connection;
+
 const handleUserInput = function(data) {
   if (data === "\u0003") {
     console.log("Closing game");
     // ctrl-c ( end of text )
     process.exit();
+  } else if (data === "w") {
+    connection.write("Move: up");
+  } else if (data === "a") {
+    connection.write("Move: left");
+  } else if (data === "s") {
+    connection.write("Move: down");
+  } else if (data === "d") {
+    connection.write("Move: right");
   }
 };
 
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -17,6 +28,6 @@ const setupInput = function() {
   });
   return stdin;
 };
-setupInput();
+
 
 module.exports = { setupInput };
