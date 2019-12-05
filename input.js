9511cb3bd212b75dbf5e9c25eef2ddb0c30708d1
Carla <cfpcarla@gmail.com>
2019-12-05 01:45:52 +0000

    separted file and export  setupInput

---
+++ input.js
@@ -0,0 +1,22 @@
+const handleUserInput = function(data) {
+  if (data === "\u0003") {
+    console.log("Closing game");
+    // ctrl-c ( end of text )
+    process.exit();
+  }
+};
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+setupInput();
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,25 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-const handleUserInput = function(data) {
-  if (data === "\u0003") {
-    console.log("Closing game");
-    // ctrl-c ( end of text )
-    process.exit();
-  }
-};
-
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", (data) => {
-    handleUserInput(data);
-  });
-  return stdin;
-};
 setupInput();
 
